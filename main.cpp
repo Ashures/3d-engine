c1ec1618afd08972faa3381f155957d181499f16
Ashures <ashuresyt@gmail.com>
2024-01-18 16:00:24 +1300

    feat: Added raylib to project

--- main.cpp
+++ main.cpp
@@ -29,8 +29,6 @@ int main(void)
 
         ClearBackground(RAYWHITE);
 
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-
         EndDrawing();
         //----------------------------------------------------------------------------------
     }
